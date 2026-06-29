0cc913b7972fcc89d9e631503b4682ea9c7dbf41
卢加龙 <451660550@qq.com>
2026-06-29 10:06:28 +0800

    fix(components): table column options extends table column props

--- packages/components/src/table/useTable/index.ts
+++ packages/components/src/table/useTable/index.ts
@@ -1,4 +1,5 @@
-import type { TableColumnCtx, TableProps } from 'element-plus'
+import type { TableProps } from 'element-plus'
+import type { TableColumnProps } from 'element-plus/es/components/table/src/table-column/defaults.mjs'
 import type { Component, VNode } from 'vue'
 import { isFunc } from '@vuetkit/shared'
 import { ElTable, ElTableColumn } from 'element-plus'
@@ -6,40 +7,21 @@ import { defineComponent, h } from 'vue'
 
 type DefaultRow = Record<PropertyKey, any>
 
-export interface TableColumn<T extends DefaultRow> {
+export interface TableColumnOptions<T extends DefaultRow> extends TableColumnProps<T> {
   // Custom render
   render?: (row: T) => VNode | string
   // Add children columns
-  children?: TableColumn<T>[]
-  // From TableColumnCtx
+  children?: TableColumnOptions<T>[]
+  // From TableColumnProps
   prop?: string
-  label?: string
-  width?: string | number
-  minWidth?: string | number
-  fixed?: boolean | 'left' | 'right'
-  align?: 'left' | 'center' | 'right'
-  headerAlign?: 'left' | 'center' | 'right'
-  className?: string
-  labelClassName?: string
-  showOverflowTooltip?: boolean
-  sortable?: boolean | string
-  sortMethod?: (a: T, b: T) => number
-  sortBy?: string | string[]
-  sortOrders?: ('ascending' | 'descending')[]
-  resizable?: boolean
-  formatter?: (row: T, column: TableColumnCtx<T>, cellValue: any, index: number) => any
-  type?: 'selection' | 'index' | 'expand'
-  selectable?: (row: T, index: number) => boolean
-  reserveSelection?: boolean
-  index?: number | ((index: number) => number)
 }
 
 export interface TableOptions<T extends DefaultRow> extends TableProps<T> {
   // Columns
-  columns: TableColumn<T>[]
-  // align
+  columns: TableColumnOptions<T>[]
+  // Align
   align?: 'left' | 'center' | 'right'
-  // header-align
+  // Header-Align
   headerAlign?: 'left' | 'center' | 'right'
 }
 
@@ -52,7 +34,7 @@ export function useTable<T extends DefaultRow>(options: TableOptions<T>): TableR
   const { columns, align, headerAlign, ...rest } = options
 
   const TableComp = defineComponent((props, { slots }) => {
-    const renderTableItemNodes = (column: TableColumn<T>) => {
+    const renderTableItemNodes = (column: TableColumnOptions<T>) => {
       // Custom render
       if (isFunc(column.render)) {
         return {
@@ -68,7 +50,7 @@ export function useTable<T extends DefaultRow>(options: TableOptions<T>): TableR
       return () => tableItemNodes
     }
 
-    const renderTableItem = (column: TableColumn<T>) => {
+    const renderTableItem = (column: TableColumnOptions<T>) => {
       const { align: columnAlign, headerAlign: columnHeaderAlign } = column
       // Extend table align and headerAlign
       const tableItemAlign = columnAlign || align || 'left'
@@ -80,7 +62,7 @@ export function useTable<T extends DefaultRow>(options: TableOptions<T>): TableR
       }, renderTableItemNodes(column))
     }
 
-    const renderTableItems = (columns: TableColumn<T>[]) => {
+    const renderTableItems = (columns: TableColumnOptions<T>[]) => {
       return columns.filter(Boolean).map((column) => {
         return renderTableItem(column)
       })
